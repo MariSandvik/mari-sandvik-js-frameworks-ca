359d03506622514e6eb0b51dee984a9979323fc0
MariSandvik <maricelina@live.com>
2020-03-29 15:36:26 +0200

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,13 +3,13 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-    firstName: yup.string().required("First name is required"),
-    lastName: yup.string().required("Last name is required"),
+    firstName: yup.string().required("First name is required").min(2, "Minimum 2 characters"),
+    lastName: yup.string().required("Last name is required").min(2, "Minimum 2 characters"),
     email: yup
         .string()
         .email("Please enter a valid email")
         .required("Email is required"),
-    message: yup.string().required("Message is required"),
+    message: yup.string().required("Message is required").min(10, "Minimum 10 characters")
 });
 
 function Contact() {
@@ -18,7 +18,6 @@ function Contact() {
     });
 
     function onSubmit(data) {
-        // this is where you can send data to an api
         console.log("data", data);
     }
 
@@ -27,8 +26,14 @@ function Contact() {
             <input name="firstName" placeholder="First name" ref={register()} />
             {errors.firstName && <p>{errors.firstName.message}</p>}
 
+            <input name="lastName" placeholder="Last name" ref={register()} />
+            {errors.lastName && <p>{errors.lastName.message}</p>}
+
             <input name="email" placeholder="Email" ref={register()} />
             {errors.email && <p>{errors.email.message}</p>}
+
+            <input name="message" placeholder="Message" ref={register()} />
+            {errors.message && <p>{errors.message.message}</p>}
 
             <input type="submit" />
         </form>
